353aade6f05120190491f17f44b590807000d13f
Sherry Fisher <sherryw17@gmail.com>
2025-01-29 11:27:22 -0600

    added jumping

--- dasher.cpp
+++ dasher.cpp
@@ -6,6 +6,13 @@ int main()
     const int windowHeight{600};
     InitWindow(windowWidth, windowHeight, "Dapper Dasher!");
 
+    // rectangle dimensions
+    const int width{50};
+    const int height{80};
+
+    int posY{windowHeight - height}; // set on ground
+    int velocity{0};
+
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
@@ -14,6 +21,14 @@ int main()
         ClearBackground(WHITE);
 
         // game logic here
+        if (IsKeyPressed(KEY_SPACE))
+        {
+            velocity -= 10;
+        }
+
+        posY += velocity;
+
+        DrawRectangle(windowWidth / 2, posY, width, height, BLUE);
 
         EndDrawing();
     }
